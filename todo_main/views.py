7f24016ce99ecabd23da4c686f8b1c8edf9ecb54
Ayush <ayushgautam647gmail.com@Ayushs-Beast.local>
2025-09-09 21:56:03 +0530

    Second Commit

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from todos.models import Task
 
 def home(request):
-      return render(request, 'home.html')
+      completed_task = Task.objects.filter(is_completed=True)
+      pending_task = Task.objects.filter(is_completed=False)
+      context = {
+            'completed_task': completed_task,
+            'pending_task': pending_task
+      }
+      return render(request, 'home.html',context)
