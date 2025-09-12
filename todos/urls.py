dec8939b78c57f8ceaadc1c0dd197a68b1c64b22
Ayush <ayushgautam647gmail.com@Ayushs-Beast.local>
2025-09-12 21:01:11 +0530

    Project Completed

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns=[
       path('addtask/',views.addtask,name='addtask'),
+      path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+      path('mark_as_undone/<int:pk>/',views.mark_as_undone,name='mark_as_undone'),
+      path('deletetask/<int:pk>/',views.deletetask,name='deletetask'),
+      path('edittask/<int:pk>/',views.edittask,name='edittask'),
+      path('save_edit_task/<int:pk>/',views.save_edit_task,name='save_edit_task'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -7,3 +7,32 @@ def addtask(request):
       task = request.POST['task']
       Task.objects.create(title=task)
       return redirect('home')
+
+def mark_as_done(request, pk):
+      task = Task.objects.get(id=pk)
+      task.is_completed = True
+      task.save()
+      return redirect('home')
+
+def mark_as_undone(request,pk):
+      task = Task.objects.get(id=pk)
+      task.is_completed = False
+      task.save()
+      return redirect('home')
+
+def deletetask(request,pk):
+      task = Task.objects.get(id=pk)
+      task.delete()
+      return redirect('home')
+
+def edittask(request,pk):
+      task = Task.objects.get(id=pk)
+      context={'task':task}
+      return render(request,'edittask.html',context)
+
+def save_edit_task(request,pk):
+      task=Task.objects.get(id=pk)
+      task.title=request.POST['task']
+      task.save()
+      return redirect('home')
+
